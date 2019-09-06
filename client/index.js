0af926ce77d5132fc4c79a0aaf6dc4a3e5db85f1
Chinedum Ukejianya <cukejianya@gmail.com>
2019-09-06 03:55:29 -0400

    Add server powered by Koa
    - shift public files to client folder
    - add index.js file
    - get api darksky to work

---
+++ client/index.js
@@ -0,0 +1,68 @@
+window.onload = () => {
+  let searchInput = document.getElementById('search-input');
+
+  searchInput.addEventListener('input', (e) => {
+    return fetch(getPlaceUrl(e.target.value), { mode: 'cors'})
+      .then(response => response.json())
+      .then(json => autoCompleteRender(json.results, e.target.value))
+  });
+}
+
+window.addEventListener('click', (e) => {
+  const autoCompleteElm = document.getElementById('autocomplete');
+  if (!e.path.includes(autoCompleteElm)) {
+    removeAllChildren(autoCompleteElm);
+    autoCompleteElm.style['border-width'] = '0px';
+  }
+});
+
+function getPlaceUrl(input) {
+  const host = 'https://places.cit.api.here.com';
+  const path = '/places/v1';
+  const resources = '/autosuggest';
+  const apiId = 'app_id=UjlYpMV8O2RNru7POxQB';
+  const apiCode = 'app_code=e_TPPvdQVgVMbu5xMYSHLQ';
+  const size = 'size=10'
+  const url = `${host}${path}${resources}?${apiId}&${apiCode}&${size}`;
+
+  return `${url}&result_types=address&q=${input}&at=39.2846225,-76.7605725`;
+}
+
+function autoCompleteRender(data, input) {
+  const autoCompleteElm = document.getElementById('autocomplete');
+  removeAllChildren(autoCompleteElm);
+  data.forEach(listRender.bind(null, input, autoCompleteElm));
+  autoCompleteElm.style['border-width'] = data.length ? '1px' : '0px';
+
+  return true;
+}
+
+function listRender(input, autoCompleteElm, place) {
+  let elm = document.createElement('div');
+  let boldRegex = new RegExp(input, 'i');
+  let breaklineRegex = /\<br\/?\>/gi;
+  let searchResult = place.title.replace(boldRegex, str => str.bold());
+  let address = place.vicinity.replace(breaklineRegex, ', ');
+  let addressHTML = `<span class="address">${address}</span>`;
+  elm.id = 'autosuggest';
+  elm.innerHTML = `${searchResult} ${addressHTML}`;
+  elm.addEventListener('click', selectLocation.bind(null, place));
+  autoCompleteElm.appendChild(elm);
+}
+
+async function selectLocation(place) {
+    let weatherObj = await fetchWeather(...place.position);
+    console.log(weatherObj);
+}
+
+function fetchWeather(lat, lng) {
+  let url = `http://127.0.0.1:3000?lat=${lat}&lng=${lng}`;
+  return fetch(url, {mode: 'cors'}).then(response => response.json());
+}
+
+function removeAllChildren(parent) {
+  while (parent.hasChildNodes()) {
+    parent.firstChild.remove();
+  }
+  return true;
+}
---
+++ server/app.js
@@ -0,0 +1,33 @@
+const fetch = require('node-fetch');
+const static = require('koa-static');
+const cors = require('@koa/cors');
+const path = require('path');
+const Koa = require('koa');
+
+
+const client = new Koa();
+const proxyServer = new Koa();
+
+client.use(static(path.join(__dirname,'../public')));
+
+proxyServer.use(async (ctx, next) => {
+  console.log('hey');
+  await next();
+  let {lat, lng} = ctx.request.query;
+  let weatherObj = await fetchWeather(lat, lng);
+  console.log(weatherObj);
+  ctx.body = weatherObj;
+});
+
+proxyServer.use(cors());
+
+
+function fetchWeather(lat, lng) {
+  let host = 'https://api.darksky.net/forecast/';
+  let apiKey = '7052606363affe26b45b0e1066113383';
+  let url = `${host}${apiKey}/${lat},${lng}`;
+  return fetch(url, {mode: 'no-cors'}).then(response => response.json());
+}
+
+proxyServer.listen(3000)
+client.listen(8080);
